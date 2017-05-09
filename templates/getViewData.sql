f35b298d905afb3f53755cc79a0bd8229b81b4e7
fxi <f@fxi.io>
2017-05-09 21:28:25 +0200

    fetch only the latest view

--- templates/getViewData.sql
+++ templates/getViewData.sql
@@ -3,4 +3,6 @@ data#>>'{"attribute","name"}' variable,
 data#>>'{"source","layerInfo","name"}' layer,
 data#>>'{"source","layerInfo","maskName"}' mask
 FROM mx_views 
-WHERE id = '{{idView}}';
+WHERE id = '{{idView}}'
+ORDER BY date_modified DESC
+LIMIT 1
